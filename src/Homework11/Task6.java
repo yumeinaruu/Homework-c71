afec6df406d02d16ef062cd24f86aeb40c1ded2c
yumeinaruu <lisovoistas@yandex.ru>
2023-11-13 18:16:11 +0300

    Initial Commit

--- src/Homework11/Task6.java
+++ src/Homework11/Task6.java
@@ -5,6 +5,29 @@ import java.util.Scanner;
 public class Task6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите слова через пробел:");
         String text = scanner.nextLine();
+        String[] words = text.split(" ");
+        System.out.println("Какое из слов вы хотите проверить на палиндром?");
+        int getWord = 0;
+        while (true) {
+            getWord = (scanner.nextInt() - 1);
+            if (getWord >= words.length || getWord < 0) {
+                System.out.println("Такого индекса нет, попробуй снова");
+            } else {
+                break;
+            }
+        }
+        char[] wordsChars = words[getWord].toCharArray();
+        for (int i = words[getWord].length() - 1; i > words[getWord].length() / 2; ) {
+            for (int j = 0; j < words[getWord].length() / 2; j++) {
+                if (wordsChars[i] != wordsChars[j]) {
+                    System.out.println("Это не палиндром");
+                    System.exit(0);
+                }
+                i--;
+            }
+        }
+        System.out.println(words[getWord] + " - палиндром");
     }
 }
