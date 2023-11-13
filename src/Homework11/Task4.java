3af48df994b2e9854b2814be1da9b965e476244e
yumeinaruu <lisovoistas@yandex.ru>
2023-11-13 19:11:11 +0300

    Initial Commit

--- src/Homework11/Task4.java
+++ src/Homework11/Task4.java
@@ -8,16 +8,21 @@ public class Task4 {
         String[] words = new String[3];
 
         for (int i = 0; i < words.length; i++) {
+            boolean hasSame = true;
             System.out.println("Введите " + (i + 1) + " слово:");
             words[i] = scanner.nextLine();
             char[] array = words[i].toCharArray();
             for (int j = 0; j < array.length - 1; j++) {
                 for (int k = j + 1; k < array.length; k++) {
                     if (array[k] == array[j]) {
-                        System.out.println(words[i]);
-                        System.exit(0);
+                        hasSame = false;
+                        break;
                     }
                 }
+            }
+            if (hasSame) {
+                System.out.println(words[i] + " - не имеет одинаковых букв");
+                System.exit(0);
             }
         }
     }
