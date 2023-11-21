72a6d8116fb00f8034f299c40c4d6cb1df5d7b37
yumeinaruu <lisovoistas@yandex.ru>
2023-11-21 16:56:42 +0300

    Initial Commit

--- src/Homework12/Task2.java
+++ src/Homework12/Task2.java
@@ -5,32 +5,27 @@ import java.util.regex.Pattern;
 
 public class Task2 {
     public static void main(String[] args) {
-        String text1 = "teachmeskills@gmail.com 1423-1512-51 +375294561234";
-        String text2 = "teachmeskills@gmail.com 1423-1512-51";
-        String text3 = "1423-1512-51 +375294561234";
-        String text4 = "teachmeskills@gmail.com +375294561234";
+        String text = "teachmeskills@gmail.com 1423-1512-51 +375294561234";
 
-        Pattern pattern = Pattern.compile("[+[\\d]{12}]");
-        Matcher matcher = pattern.matcher(text1);
-        //[[A-z]@[a-z].[a-z]]
-        //[[A-z]{1,}@[a-z]{1,}.[a-z]{1,}][+[\d]{12}]
-        //[[\d{4}\-\d{4}-]\d{2}]
+        Pattern pattern = Pattern.compile("\\+\\d{12}");
+        Matcher matcher = pattern.matcher(text);
+        System.out.println("Number: ");
         while (matcher.find()){
             System.out.print(matcher.group());
         }
-        System.out.println();
-        matcher = pattern.matcher(text2);
+
+        pattern = Pattern.compile("[A-z]+@[A-z]+\\.[A-z]+");
+        matcher = pattern.matcher(text);
+        System.out.println("\nEmail: ");
         while (matcher.find()){
             System.out.print(matcher.group());
         }
-        System.out.println();
-        matcher = pattern.matcher(text3);
+
+        pattern = Pattern.compile("\\d{4}-\\d{4}-\\d{2}");
+        System.out.println("\nDocument number: ");
+        matcher = pattern.matcher(text);
         while (matcher.find()){
-            System.out.print(matcher.group());
-        }
-        matcher = pattern.matcher(text4);
-        while (matcher.find()){
-            System.out.print(matcher.group());
+            System.out.println(matcher.group());
         }
     }
 }
