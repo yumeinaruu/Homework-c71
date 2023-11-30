432038df4a78f7ceeea0fbeb5b71773e4741ab8c
Yumeinaruu <lisovoistas@yandex.ru>
2023-11-30 17:22:31 +0300

    Initial Commit

--- src/Homework15/task3/Main.java
+++ src/Homework15/task3/Main.java
@@ -1,5 +1,6 @@
 package Homework15.task3;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -11,11 +12,15 @@ public class Main {
         int len = scanner.nextInt();
         for (int i = 0; i < len; i++) {
             System.out.println("Добавление " + (i+1) + " студента:");
-            linkedList.add(new Student(scanner.nextLine(), scanner.nextLine(), scanner.nextInt()));
-            System.out.println("Введите оценки студента: ");
-            linkedList.get(i).setMarks(linkedList, linkedList.get(i));
+            System.out.println("Введите имя, группу и номер курса студента:");
+            String name = scanner.next();
+            int group = scanner.nextInt();
+            int course = scanner.nextInt();
+            linkedList.add(new Student(name, group, course));
+            linkedList.get(i).setMarks(linkedList.get(i));
             linkedList.get(i).DeleteStudent(linkedList, linkedList.get(i));
         }
-        System.out.println(linkedList);
+        System.out.println("Какой курс вам нужен?");
+        Student.printStudents(linkedList, scanner.nextInt());
     }
 }
--- src/Homework15/task3/Student.java
+++ src/Homework15/task3/Student.java
@@ -1,27 +1,30 @@
 package Homework15.task3;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Student {
     Scanner scanner = new Scanner(System.in);
     private String name;
-    private String group;
+    private int group;
     private int course;
     private int[] marks;
 
-    public void setMarks(LinkedList<Student> linkedList, Student student) {
-        System.out.println("Введите количество оценок: ");
-        int len = scanner.nextInt();
-        for (int i = 0; i < len; i++) {
-            student.marks[i] = scanner.nextInt();
-        }
-    }
-
-    public Student(String name, String group, int course) {
+    public Student(String name, int group, int course) {
         this.name = name;
         this.group = group;
         this.course = course;
+    }
+
+    public void setMarks(Student student) {
+        System.out.println("Введите количество оценок: ");
+        int len = scanner.nextInt();
+        marks = new int[len];
+        for (int i = 0; i < len; i++) {
+            System.out.println("Введите оценку:");
+            student.marks[i] = scanner.nextInt();
+        }
     }
 
     public void DeleteStudent(LinkedList<Student> linkedList, Student student) {
@@ -31,6 +34,8 @@ public class Student {
             sum += marks[j];
         }
 
+        sum = sum/student.marks.length;
+
         if (sum >= 3) {
             System.out.println("Вы прошли на следующий курс!");
             student.course++;
@@ -38,5 +43,23 @@ public class Student {
             System.out.println("Вы отчислены!");
             linkedList.remove(student);
         }
+    }
+
+    public static void printStudents(LinkedList<Student> linkedList, int course) {
+        for (int i = 0; i < linkedList.toArray().length; i++) {
+            if (linkedList.get(i).course == course) {
+                System.out.println("Студент: " + linkedList.get(i).toString() + " на " + course + " курсе");
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                ", name='" + name + '\'' +
+                ", group=" + group +
+                ", course=" + course +
+                ", marks=" + Arrays.toString(marks) +
+                '}';
     }
 }
