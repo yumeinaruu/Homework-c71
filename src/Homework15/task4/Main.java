d07e4c3fdd8648b1fe66e31d863e12edd98045f5
Yumeinaruu <lisovoistas@yandex.ru>
2023-11-30 17:28:26 +0300

    Initial Commit

---
+++ src/Homework15/task4/Main.java
@@ -0,0 +1,7 @@
+package Homework15.task4;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Homework15/task4/MyArrayList.java
@@ -0,0 +1,5 @@
+package Homework15.task4;
+
+public class MyArrayList <T>{
+
+}
