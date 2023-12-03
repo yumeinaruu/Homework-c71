b7b0e665f2bd8f130cca808e7185dfd10e5b07fb
Yumeinaruu <lisovoistas@yandex.ru>
2023-12-03 14:45:57 +0300

    Initial Commit

--- src/Homework15/task4/Main.java
+++ src/Homework15/task4/Main.java
@@ -2,6 +2,29 @@ package Homework15.task4;
 
 public class Main {
     public static void main(String[] args) {
+        MyArrayList<Integer> myArrayList = new MyArrayList<>();
+        myArrayList.add(myArrayList, 1);
+        myArrayList.add(myArrayList, 2);
+        myArrayList.add(myArrayList, 3);
+        myArrayList.add(myArrayList, 4);
+        myArrayList.add(myArrayList, 5);
+        myArrayList.add(myArrayList, 6);
+        myArrayList.add(myArrayList, 7);
+        myArrayList.add(myArrayList, 8);
+        myArrayList.add(myArrayList, 9);
+        myArrayList.add(myArrayList, 10);
+        myArrayList.add(myArrayList, 11);
+        System.out.println(myArrayList);
+        myArrayList.remove(myArrayList, 1);
+        myArrayList.remove(myArrayList, 3);
+        System.out.println(myArrayList);
+        System.out.println(myArrayList.get(myArrayList, 4));
+        System.out.println(myArrayList.contains(myArrayList, 8));
+        System.out.println(myArrayList.contains(myArrayList, 45));
+        myArrayList.clean(myArrayList);
+        System.out.println(myArrayList);
 
+        MyArrayList<Integer> myArrayList1 = new MyArrayList<>(5);
+        System.out.println(myArrayList1);
     }
 }
--- src/Homework15/task4/MyArrayList.java
+++ src/Homework15/task4/MyArrayList.java
@@ -1,5 +1,57 @@
 package Homework15.task4;
 
-public class MyArrayList <T>{
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class MyArrayList<T> {
+    static Object[] elementData;
+    private int size = 10;
+    private int s = 0;
+
+    public MyArrayList() {
+        this.elementData = new Object[size];
+    }
+
+    public MyArrayList(int size) {
+        this.elementData = new Object[size];
+    }
+
+    public void add(MyArrayList<T> myArrayList, T t) {
+        if (s == MyArrayList.elementData.length) {
+            MyArrayList.elementData = Arrays.copyOf(MyArrayList.elementData, elementData.length * 2);
+            MyArrayList.elementData[s] = t;
+            s++;
+        } else {
+            MyArrayList.elementData[s] = t;
+            s++;
+        }
+    }
+
+    public void remove(MyArrayList<T> myArrayList, int index) {
+        for(int i = index; i < elementData.length - 1; i++){
+            elementData[i] = elementData[i+1];
+        }
+    }
+
+    public T get(MyArrayList<T> myArrayList, int index) {
+        return (T) elementData[index];
+    }
+
+    public boolean contains(MyArrayList<T> myArrayList, T container) {
+        for (int i = 0; i < elementData.length; i++) {
+            if (elementData[i] == container) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void clean(MyArrayList<T> myArrayList) {
+        Arrays.fill(elementData, null);
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(elementData);
+    }
 }
